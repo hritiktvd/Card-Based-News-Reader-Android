194d018f394c327d78f18aedd8bdafdd4aee9d5c
hriti <hritik2599@gmail.com>
2019-06-12 17:39:09 +0530

    seventh commit.working Article layout with image.

--- app/src/main/java/com/hritik/articlereader/Article_main.java
+++ app/src/main/java/com/hritik/articlereader/Article_main.java
@@ -1,11 +1,14 @@
 package com.hritik.articlereader;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -22,10 +25,16 @@ public class Article_main extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_article);
 
+        getSupportActionBar().setTitle("ARTICLE READER"); // for set actionbar title
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true); // for add back arrow in action bar
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#09061a")));
+
+
         //changing the color of status bar.
         if (Build.VERSION.SDK_INT >= 21) {
             getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.colorPrimaryDark)); //status bar or the time bar at the top
         }
+
 
         coverImage = findViewById(R.id.coverImage);
         articletitle = findViewById(R.id.articletitle);
@@ -41,7 +50,16 @@ public class Article_main extends AppCompatActivity {
         articletitle.setText(showtitle);
         articledesc.setText(showdesc);
 
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // TODO Auto-generated method stub
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
 
     }
 }
